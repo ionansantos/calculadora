8c83cf07d02fc2bc00f688462cb2dd3c42b51f91
ionansantos <https://github.com/ionansantos>
2021-07-08 14:09:55 -0300

    att

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+ git push -f git@github.com:ionansantos/calculadora.git master:gh-pages
+
+cd -
